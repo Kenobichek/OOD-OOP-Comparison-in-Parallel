0c594ed3978f27864d332926c89ab3833ac179ca
Olesia Lesechko <lesechklesia@gmail.com>
2024-04-28 20:34:12 +0300

    added oop inheritance structure

--- inheritance_version/src/main.cc
+++ inheritance_version/src/main.cc
@@ -1,5 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath> 
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "game.h"
 
 int main() {
-	std::cout << "Hello, world!!!";
+
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(800, 800, "ESC", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+    Game game;
+	// game.AddObject(std::make_unique<Circle>(0, 0, 0.01f, 0, 0.3f));
+	game.AddObject(std::make_unique<Square>(0, 0, 0.01f, 0, 0.5));
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		game.Update();
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		game.Draw();
+
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
